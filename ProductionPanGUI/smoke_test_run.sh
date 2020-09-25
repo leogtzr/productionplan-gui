aaa2dabcffaad193b7f811748f0ca4d3e58ef0dd
leogtzr <leogutierrezramirez@gmail.com>
2020-09-25 15:13:58 -0600

    fix smoke test script to run the program without problems

--- ProductionPanGUI/smoke_test_run.sh
+++ ProductionPanGUI/smoke_test_run.sh
@@ -4,9 +4,8 @@ set -x
 readonly work_dir="$(dirname $(readlink --canonicalize-existing "${0}"))"
 readonly dist_dir="${work_dir}/dist"
 readonly jar_file="ProductionPanGUI.jar"
-cp --force "${HOME}/inputs/FAB Load by WC Leo.xls" dist/
-cp --force "${HOME}/inputs/Age  by WC.xls" dist/
-cp --force "${HOME}/inputs/part-machine"* dist/
+
+cp --force "${work_dir}"/run/* dist/
 
 (
 	cd "${work_dir}/dist"
