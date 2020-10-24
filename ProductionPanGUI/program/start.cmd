11ccdb44ed57d175b9d319839e837882e774833d
Gutierrez <Leonardo.Gutierrez@autozone.com>
2020-10-23 23:42:55 -0600

    change to generate files in output directory

--- ProductionPanGUI/program/start.cmd
+++ ProductionPanGUI/program/start.cmd
@@ -1,5 +1,8 @@
 @echo off
 
-jdk-14.0.2\bin\java.exe -jar "ProductionPanGUI.jar" run\ >> debug.log 2>&1
+mkdir output
+jdk-14.0.2\bin\java.exe -jar "ProductionPanGUI.jar" output\ >> debug.log 2>&1
+
+pause
 
 exit /b 0
